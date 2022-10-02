e278b678990f2b36bb4f4aaf2d74f1c9f70981cc
dddiewo <yyy000803@naver.com>
2022-10-02 14:12:35 +0900

    homeController생성userController수정
    회원가입, 회원탈퇴 함수생성
    홈컨트롤러에는 메인페이지에 필요한 함수들 넣고 연결된 뷰 랜더 연결 시킬 예정
    하기 너무 시러시러시러

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,15 +1,46 @@
-const db= require("../models/index.js"),
-    User= db.metaUser,
-    getUserParamas = body =>{
-        return{
-            nickname: body.first,
-            userEmail: body.last,
-            password: body.password
-            //나중에 뱉어내도록
 
-        };
-    };
+const models = require("../models/index"),
+    User= models.metaUser;
+    Op = models.Sequelize.Op;
 
+
+
+exports.signUp = async (res, req) => { //회원가입 함수
+
+        models.User.create({  
+                userEmail: req.body.userEmail,
+                userName: req.body.userName,
+                userpwd: req.body.userpwd
+        })
+        .then( result => {
+                console.log("회원가입 완료");
+                
+        })
+        .catch( err => {
+                console.log(err)
+                console.log("회원가입 실패");
+                res.send("<script>alert('이미 사용중인 이메일입니다.');location.href='/signUp';</script>");
+        })
+};
+
+
+exports.userDelete = async (req, res) => {   //회원 탈퇴
+    try {
+        console.log("UserDelete 중 삭제할 Id", req.session.idx)
+        await User.destroy({ //세션에 저장된 id로 memberId를 찾아 destroy 해준다.
+            where:{
+                userID : req.session.idx
+            }
+        });
+        
+        console.log("회원 삭제 완료")
+        //탈퇴 완료 view로 랜더링
+    
+    } catch(err) {
+    //    res.render(블라블라) //오류 view로  보내준다.(for user)
+        console.log(err); //for developer
+    }
+}
 
     const getNickname = async(id) => {
         try{
